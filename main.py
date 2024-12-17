f401d1f16c3b4c8ad07659b8582321a1e1807902
Jailjac <jacobmazac@gmail.com>
2024-12-16 23:47:32 -0800

    added rotation controls

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while(True):
         for event in pygame.event.get():
@@ -16,6 +15,8 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
